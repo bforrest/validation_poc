f855dce79a695ec9c24c2cfb9aa125a650012169
Barry Forrest <bforrest30@gmail.com>
2015-08-28 16:55:04 -0500

    over ride default name

--- Validation/BankAccountValidator.cs
+++ Validation/BankAccountValidator.cs
@@ -7,16 +7,16 @@ namespace Validation
         public BankAccountValidator()
         {
             RuleFor(account => account.AccountNumber).NotEmpty().Length(1, 40)
-                .WithMessage("Account number is required and must be less than 40 characters.");
+                .WithName("Account Number");
 
             RuleFor(account => account.BankAccountName).NotEmpty().Length(1, 254)
-                .WithMessage("Bank Account Name is required and must be less than 254 characters.");
+                .WithName("Bank Account Name");
             
             RuleFor(account => account.ABA).Length(0, 20)
                 .WithMessage("ABA should be less than 20 characters.");
             
             RuleFor(account => account.BankName).Length(0, 254)
-                .WithMessage("Bank Name must be less than 254 characters.");
+                .WithName("Bank Name");
         }
     }
 }
