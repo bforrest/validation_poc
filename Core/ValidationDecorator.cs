988110a71119dba33ce37b646e7802e23648d895
Barry Forrest <bforrest30@gmail.com>
2015-09-08 08:56:40 -0500

    text input hinting

---
+++ Core/ValidationDecorator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.DynamicProxy;
+
+namespace Core
+{
+    public class ValidationDecorator : IInterceptor
+    {
+        public void Intercept(IInvocation invocation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
